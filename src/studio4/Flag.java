7a24f6a209ff27e34819a1581aff772f528e38a0
Aaron Spektor <AaronSpektor@127.0.0.1>
2022-10-12 16:46:11 -0500

    hopefully this fixes things

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,7 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(255, 255, 355);
+		StdDraw.filledRectangle(3, 3, 3, 3);
 	}
 }
