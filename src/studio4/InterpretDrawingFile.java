de3d23a66e5798214d387971a43eb959fd0b0b45
Aaron Spektor <AaronSpektor@127.0.0.1>
2022-10-12 16:35:05 -0500

    finished making the class

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,5 @@
 package studio4;
-
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +19,61 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		
+		Color color = new Color(redComponent, greenComponent, blueComponent);
+		StdDraw.setPenColor(color);
+		
+		
+		if(shapeType.equals("triangle")) {
+			double x1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			
+			double y1 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double y3 = in.nextDouble();
+			
+			double[] xCoords = {x1, x2, x3};
+			double[] yCoords = {y1, y2, y3};
+			
+			if(isFilled) {
+				StdDraw.filledPolygon(xCoords, yCoords);
+			}else {
+				StdDraw.polygon(xCoords, yCoords);
+			}
+			
+		}else if(shapeType.equals("rectangle")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			
+			if(isFilled) {
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			}else {
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+		}else if(shapeType.equals("ellipse")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			
+			if(isFilled) {
+				StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
+			}else {
+				StdDraw.ellipse(x, y, halfWidth, halfHeight);
+			}
+		}
+		
+		
+		
+		
 		
 	}
 }
